7403070cca778f767fa3aa70a0d5aec068b551d4
Whelpley <whelpley@gmail.com>
2017-04-04 14:50:00 -0700

    Create separate, simple app to rebuild from

---
+++ js/effects_simple.js
@@ -0,0 +1,188 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+
+var audioInput = null;
+var effectInput = null;
+
+var wetGain = null; 
+var dryGain = null;
+
+// the sound effect currently selected
+var currentEffectNode = null;
+
+var dtime = null; // in createDelay()
+var dregen = null; // in createDelay()
+
+// for selecting audio input
+var constraints = 
+{
+  audio: {
+      optional: [{ echoCancellation: false }]
+  }
+};
+
+function convertToMono( input ) {
+    var splitter = audioContext.createChannelSplitter(2);
+    var merger = audioContext.createChannelMerger(2);
+
+    input.connect( splitter );
+    splitter.connect( merger, 0, 0 );
+    splitter.connect( merger, 0, 1 );
+    return merger;
+}
+
+// main processing function for audio nodes
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+
+    var input = audioContext.createMediaStreamSource(stream);
+    // moved out of global scope
+    var outputMix = null;
+
+
+    audioInput = convertToMono( input );
+
+    // Reformed sequence of connections - no visuals
+    outputMix = audioContext.createGain();
+    dryGain = audioContext.createGain();
+    wetGain = audioContext.createGain();
+    effectInput = audioContext.createGain();
+
+    audioInput.connect(dryGain);
+    audioInput.connect(effectInput);
+    dryGain.connect(outputMix);
+    wetGain.connect(outputMix);
+    outputMix.connect( audioContext.destination);
+
+    changeEffect(); // Initiates first sound effect
+
+}
+
+
+function gotSources(sourceInfos) {
+    var audioSelect = document.getElementById("audioinput");
+    while (audioSelect.firstChild)
+        audioSelect.removeChild(audioSelect.firstChild);
+
+    for (var i = 0; i != sourceInfos.length; ++i) {
+        var sourceInfo = sourceInfos[i];
+        if (sourceInfo.kind === 'audio') {
+            var option = document.createElement("option");
+            option.value = sourceInfo.id;
+            option.text = sourceInfo.label || 'input ' + (audioSelect.length + 1);
+            audioSelect.appendChild(option);
+        }
+    }
+}
+
+function initAudio() {
+    var irRRequest = new XMLHttpRequest();
+    irRRequest.open("GET", "sounds/cardiod-rear-levelled.wav", true);
+    irRRequest.responseType = "arraybuffer";
+    irRRequest.onload = function() {
+        audioContext.decodeAudioData( irRRequest.response, 
+            function(buffer) { 
+                // reverbBuffer = buffer; 
+            } );
+    }
+    irRRequest.send();
+
+    if (!navigator.getUserMedia)
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    if (!navigator.getUserMedia)
+        return(alert("Error: getUserMedia not supported!"));
+
+    // IF we can getUserMedia, call gotStream
+    navigator.getUserMedia(constraints, gotStream, function(e) {
+            alert('Error getting audio');
+            console.log(e);
+        });
+
+    if ((typeof MediaStreamTrack === 'undefined')||(!MediaStreamTrack.getSources)){
+        console.log("This browser does not support MediaStreamTrack, so doesn't support selecting sources.\n\nTry Chrome Canary.");
+    } else {
+        MediaStreamTrack.getSources(gotSources);
+    }
+
+    // Activates selection from effects menu
+    // document.getElementById("effect").onchange=changeEffect;
+}
+
+function keyPress(ev) {
+    var oldEffect = document.getElementById("effect").selectedIndex;
+    var newEffect = oldEffect;
+    switch (ev.keyCode) {
+      case 50: // 'r'
+        newEffect = 1;
+        break;
+      case 49: // 'c'
+        newEffect = 8;
+        break;
+      case 51: // 'p'
+        newEffect = 10;
+        break;
+      default:
+        console.log(ev.keyCode);
+    }
+    if (newEffect != oldEffect) {
+        document.getElementById("effect").selectedIndex = newEffect;
+        changeEffect();
+    }
+}
+
+// Start off the audio
+window.addEventListener('load', initAudio );
+
+// Listen for user input
+window.addEventListener('keydown', keyPress );
+
+// should this be in global scope?
+var lastEffect = -1;
+
+function changeEffect() {
+    dtime = null;
+    dregen = null;
+    
+    // disengage with previous effects
+    if (currentEffectNode) 
+        currentEffectNode.disconnect();
+    if (effectInput)
+        effectInput.disconnect();
+
+
+    var effect = document.getElementById("effect").selectedIndex;
+    var effectControls = document.getElementById("controls");
+    if (lastEffect > -1)
+        effectControls.children[lastEffect].classList.remove("display");
+    lastEffect = effect;
+    effectControls.children[effect].classList.add("display");
+
+    switch (effect) {
+        case 0: // Delay
+            currentEffectNode = createDelay();
+            break;
+     
+        default:
+            break;
+    }
+    audioInput.connect( currentEffectNode );
+}
+
+
+function createDelay() {
+    var delayNode = audioContext.createDelay();
+
+    delayNode.delayTime.value = parseFloat( document.getElementById("dtime").value );
+    dtime = delayNode;
+
+    var gainNode = audioContext.createGain();
+    gainNode.gain.value = parseFloat( document.getElementById("dregen").value );
+    dregen = gainNode;
+
+    gainNode.connect( delayNode );
+    delayNode.connect( gainNode );
+    delayNode.connect( wetGain );
+
+    return delayNode;
+}
