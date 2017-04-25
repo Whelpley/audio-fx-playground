6598a59ca17ead5176f72392a2bbf78d6beb0f8e
Whelpley <whelpley@gmail.com>
2017-04-25 14:45:54 -0700

    Second slider

--- angular-version/src/app/mock-effects.ts
+++ angular-version/src/app/mock-effects.ts
@@ -1,14 +1,14 @@
 import { Effect } from './effect';
 
 export var EFFECTS: Effect[] = [
-  {id: 11, name: 'Delay', delayTime: 5, delayRegen: 3},
-  {id: 12, name: 'Reverb', delayTime: 5, delayRegen: 3},
-  {id: 13, name: 'Chipmunk', delayTime: 5, delayRegen: 3},
-  {id: 14, name: 'Darth Vader', delayTime: 5, delayRegen: 3},
-  {id: 15, name: 'Old Lady', delayTime: 5, delayRegen: 3},
-  {id: 16, name: 'Old Man', delayTime: 5, delayRegen: 3},
-  {id: 17, name: 'Elder God', delayTime: 5, delayRegen: 3},
-  {id: 18, name: 'Fairy', delayTime: 5, delayRegen: 3},
-  {id: 19, name: 'Background ChodelayTimerus', delayTime: 5, delayRegen: 3},
-  {id: 20, name: 'Crackly Telephone', delayTime: 5, delayRegen: 3}
+  {id: 11, name: 'Delay', delayTime: 1, delayRegen: 3},
+  {id: 12, name: 'Reverb', delayTime: 1, delayRegen: 3},
+  {id: 13, name: 'Chipmunk', delayTime: 1, delayRegen: 3},
+  {id: 14, name: 'Darth Vader', delayTime: 1, delayRegen: 3},
+  {id: 15, name: 'Old Lady', delayTime: 1, delayRegen: 3},
+  {id: 16, name: 'Old Man', delayTime: 1, delayRegen: 3},
+  {id: 17, name: 'Elder God', delayTime: 1, delayRegen: 3},
+  {id: 18, name: 'Fairy', delayTime: 1, delayRegen: 3},
+  {id: 19, name: 'Background Chorus', delayTime: 1, delayRegen: 3},
+  {id: 20, name: 'Crackly Telephone', delayTime: 1, delayRegen: 3}
 ];
